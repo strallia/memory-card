de1fdd37ab2fa8bb394bdd450f7840739b4d417e
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-04-15 22:09:48 -0700

    feat: Fetch and display card images

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
 import { Header } from "./components/Header";
 import { GameTable } from "./components/GameTable";
 import { Footer } from "./components/Footer";
+import { useEffect, useState } from "react";
+
+const baseURL = "https://pokeapi.co/api/v2/pokemon/";
+const pokemonNames = [
+  "pikachu",
+  "charizard",
+  "squirtle",
+  "charmander",
+  "lucario",
+  "dragonite",
+  "umbreon",
+  "eevee",
+  "blastoise",
+  "gardevoir",
+];
 
 function App() {
+  const [imageURLs, setImageURLs] = useState([]);
+
+  useEffect(() => {
+    const allFetchedImageURLs = pokemonNames.map((pokemon) =>
+      fetch(`${baseURL}/${pokemon}`, { mode: "cors" })
+    );
+    Promise.all(allFetchedImageURLs)
+      .then((results) => Promise.all(results.map((r) => r.json())))
+      .then((results) => {
+        const updatedImageURLs = results.map(
+          (result) => result.sprites.front_default
+        );
+        setImageURLs(updatedImageURLs);
+      });
+  }, []);
+
   return (
     <>
       <Header />
-      <GameTable />
+      <GameTable imageURLs={imageURLs} />
       <Footer />
     </>
   );
--- src/components/GameTable.jsx
+++ src/components/GameTable.jsx
@@ -1,5 +1,11 @@
-function GameTable() {
-  return null;
+function GameTable({ imageURLs }) {
+  return (
+    <>
+      {imageURLs.map((url, index) => {
+        return <img key={index} src={url} />;
+      })}
+    </>
+  );
 }
 
 export { GameTable };
